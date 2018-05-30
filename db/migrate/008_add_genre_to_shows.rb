7612d916b2b031d6b39b1794eae531da2c25b9ec
Eddie Moon <eddiemoonie@gmail.com>
2018-05-30 20:49:24 +0000

    automatically backed up by learn

--- db/migrate/008_add_genre_to_shows.rb
+++ db/migrate/008_add_genre_to_shows.rb
@@ -1,5 +1,5 @@
-class AddCatchphraseToCharacter < ActiveRecord::Migration[4.2]
+class AddGenreToShows < ActiveRecord::Migration[4.2]
   def change
-    add_column :genre
+    add_column :shows, :genre, :string
   end
 end
